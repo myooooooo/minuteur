92dc181ad7ef1af9118cdd08d7b166a368a99dd2
Zineb Anssafou <oufassna@gmail.com>
2026-03-06 13:54:13 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by zineb on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
